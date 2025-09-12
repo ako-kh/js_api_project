f8f1402685ef154c9d6fdffdaf450efee52aa6a9
MAMAMI333 <xoravaakaki989@gmail.com>
2025-09-12 16:39:49 +0200

    Enhance cart functionality: update item display and improve add-to-cart logic

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const HEADERS = {headers: {"Authorization": "Bearer " + window.localStorage.getItem("access_token")}}
 
-function addToCart_f(product_id) {
+function addToCart_f(product_id, quantity=1) {
     axios.post("https://api.everrest.educata.dev/shop/cart/product",{
         id: product_id,
-        quantity: 1,
+        quantity: quantity,
     }, HEADERS
     ).then(function(response){
         alert("product added to cart")
     }).catch(function(error){
         console.log(error)
+        axios.patch("https://api.everrest.educata.dev/shop/cart/product",{
+            id: product_id,
+            quantity: quantity,
+        }, HEADERS
+        ).then(function(response){
+            alert("product added to cart")
+        }).catch(function(error){
+            console.log(error)
+        })
     })
 }
